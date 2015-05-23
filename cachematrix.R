dade92e1e9b2bc2fb861cf3028d564692c6cf5f8
Unlimited Blade Works <danaplewis@gmail.com>
2015-05-23 11:16:53 -0400

    Cached matrix utilities

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates a 'cached' version of a matrix, whose inverse will be
+## stored between requests as long as the matrix is unchanged.
 
 makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    get <- function() { x }
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    getinv <- function() { inv }
+    setinv <- function(i) { inv <<- i }
 
+    list(get = get, set = set, getinv = getinv, setinv = setinv)
 }
 
 
-## Write a short comment describing this function
+## Returns a cached matrix's inverse, recomputing it if necessary.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinv()
+    if (is.null(i)) {
+        xmat <- x$get()
+        i <- solve(xmat, ...)
+        x$setinv(i)
+    } else {
+        message("getting cached inverse")
+    }
+    i
 }
